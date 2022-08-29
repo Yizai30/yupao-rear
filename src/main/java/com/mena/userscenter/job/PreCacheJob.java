6e7c4142b3bd5ada2e8cabe6f32e51afe3a3449f
Menamovic <1362800827@qq.com>
2022-08-29 21:26:57 +0800

    update PreCacheJob.java 通过 redisson 为定时任务加分布式锁，保证同一个定时任务在同一时间仅有一个服务器能执行

--- src/main/java/com/mena/userscenter/job/PreCacheJob.java
+++ src/main/java/com/mena/userscenter/job/PreCacheJob.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mena.userscenter.model.domain.User;
 import com.mena.userscenter.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -30,22 +32,40 @@ public class PreCacheJob {
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Resource
+    private RedissonClient redissonClient;
+
     // 重点用户（暂时写死，可用配置动态指定），数据库可以加一个标识，每次去查重点用户
     private List<Long> mainUserList = Arrays.asList(7L);
 
     // 每天执行，预热推荐用户
-    @Scheduled(cron = "0 46 12 * * *")
+    @Scheduled(cron = "0 16 20 * * *")
     public void doCacheRecommendUser() {
-        for (Long userId : mainUserList) {
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
-            String redisKey = String.format("yizai:user:recommend:%s", userId);
-            ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-            // 写缓存
-            try {
-                valueOperations.set(redisKey, userPage, 30000, TimeUnit.MILLISECONDS);
-            } catch (Exception e) {
-                log.error("redis set key error", e);
+        RLock lock = redissonClient.getLock("yizai:precachejob:docache:lock");
+        try {
+            // 只有一个线程能够获取到值
+            if (lock.tryLock(0, 30000L, TimeUnit.MILLISECONDS)) {
+                System.out.println("CurrentThreadId: " + Thread.currentThread().getId());
+                for (Long userId : mainUserList) {
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
+                    String redisKey = String.format("yizai:user:recommend:%s", userId);
+                    ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+                    // 写缓存
+                    try {
+                        valueOperations.set(redisKey, userPage, 30000, TimeUnit.MILLISECONDS);
+                    } catch (Exception e) {
+                        log.error("redis set key error", e);
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            log.error("doCacheRecommendUser error ", e);
+        } finally {
+            // 只能释放自己的锁
+            if (lock.isHeldByCurrentThread()) {
+                System.out.println("CurrentThreadId: " + Thread.currentThread().getId());
+                lock.unlock();
             }
         }
     }
