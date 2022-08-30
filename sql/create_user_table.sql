6b66fa4c6b1f1863d9c3d5a071c7df1811e7b61c
Menamovic <1362800827@qq.com>
2022-08-30 13:15:29 +0800

    update create_user_table.sql 存储新的建表语句

--- sql/create_user_table.sql
+++ sql/create_user_table.sql
@@ -1,12 +1,12 @@
--- auto-generated definition
+-- 用户表
 create table user
 (
-    id           bigint auto_increment comment 'id'
-        primary key,
+    id           bigint auto_increment comment 'id' primary key,
     username     varchar(256)                       null comment '用户昵称',
     userAccount  varchar(256)                       null comment '账号',
     avatarUrl    varchar(1024)                      null comment '用户头像',
     gender       tinyint                            null comment '性别',
+    userProfile  varchar(512)                       null comment '个人简介',
     userPassword varchar(512)                       not null comment '密码',
     phone        varchar(128)                       null comment '电话',
     email        varchar(512)                       null comment '邮箱',
@@ -17,14 +17,12 @@ create table user
     userRole     int      default 0                 not null comment '用户角色：0-普通用户，1-管理员',
     planetCode   varchar(512)                       null comment '星球编号',
     tags         varchar(1024)                      null comment '标签 json 列表'
-)
-    comment '用户';
+) comment '用户';
 
--- auto-generated definition
+-- 标签表
 create table tag
 (
-    id         bigint auto_increment comment 'id'
-        primary key,
+    id         bigint auto_increment comment 'id' primary key,
     tagName    varchar(256)                       null comment '标签名称',
     userId     bigint                             null comment '用户 id',
     parentId   bigint                             null comment '父标签 id',
@@ -34,9 +32,35 @@ create table tag
     isDelete   tinyint  default 0                 not null comment '是否删除',
     constraint uniIdx_tagName
         unique (tagName)
-)
-    comment '标签';
+) comment '标签';
 
 create index idx_userId
     on tag (userId);
 
+-- 队伍表
+create table team
+(
+    id           bigint auto_increment comment 'id' primary key,
+    name  			 varchar(256)                       not null comment '队伍名称',
+    description  varchar(1024)                      null comment '描述',
+    maxNum   		 int      default 1                 not null comment '最大人数',
+    expireTime   datetime														null comment '过期时间',
+    userId       bigint														  comment '用户id',
+    userStatus   int      default 0                 not null comment '0-公开，1-私有，2-加密',
+    password 		 varchar(512)                       null comment '密码',
+    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint  default 0                 not null comment '是否删除'
+) comment '队伍';
+
+-- 用户队伍关系表
+create table user_team
+(
+    id           bigint auto_increment comment 'id' primary key,
+    userId       bigint														  comment '用户id',
+    teamId       bigint														  comment '队伍id',
+    joinTime   	 datetime  													null comment '加入时间',
+    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint  default 0                 not null comment '是否删除'
+) comment '用户队伍关系';
