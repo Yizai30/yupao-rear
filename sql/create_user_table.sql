66b91adddb79eb059332e132c40044bbcf2047ca
Menamovic <1362800827@qq.com>
2022-08-31 12:04:53 +0800

    update create_user_table.sql 修正队伍表字段 userStatus -> status

--- sql/create_user_table.sql
+++ sql/create_user_table.sql
@@ -46,7 +46,7 @@ create table team
     maxNum   		 int      default 1                 not null comment '最大人数',
     expireTime   datetime														null comment '过期时间',
     userId       bigint														  comment '用户id',
-    userStatus   int      default 0                 not null comment '0-公开，1-私有，2-加密',
+    status   int      default 0                 not null comment '0-公开，1-私有，2-加密',
     password 		 varchar(512)                       null comment '密码',
     createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
     updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
